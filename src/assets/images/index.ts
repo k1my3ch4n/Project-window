435267208398efa3a05cf2f4e0853a324869640d
yechan <kevin2695@naver.com>
2024-03-25 21:53:42 +0900

    [240325] 이미지 추가

---
+++ src/assets/images/index.ts
@@ -0,0 +1,1 @@
+export { ReactComponent as Window } from "./window.svg";
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,25 @@
 /// <reference types="vite/client" />
+
+declare module "@images";
+
+declare module "*.svg" {
+  import React = require("react");
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare const context: jest.Describe;
+
+interface ImportMetaEnv {
+  readonly VITE_URI?: string;
+  readonly VITE_WS_URI?: string;
+  readonly VITE_REST_URI?: string;
+  readonly VITE_CLIENT_ID?: string;
+  readonly VITE_CLIENT_SECRET?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
